f9b7581eb8ac9d2fb4abf3527511848bb5153a33
luanmgdo <luanmacedo108@gmail.com>
2017-08-28 10:21:33 -0300

    atualizado

--- capiuca.c
+++ capiuca.c
@@ -1,31 +1,43 @@
 #include <stdio.h>
 
 int inversao(int x){
-    int numinvertido=0, centena=0, dezena=0, unidade=0;
-    printf("oi\n");
-    centena=x/100;
-    dezena=(x%100)/10;
-    unidade=(x%100)%10;
-    numinvertido=(unidade*100)+(dezena*10)+centena;
-    printf("numero: %d\n", numinvertido);
-    return numinvertido;
+    int milhar = 0, centena = 0, dezena = 0, unidade = 0, invertido = 0;    
+    if(x > 10)
+      {
+        milhar = x % 10;
+        x = x / 10;
+        centena = x % 10;
+        x = x / 10;
+        dezena = x % 10;
+        x = x / 10;
+        unidade = x % 10;
+      }
+    invertido = (milhar*1000) + (centena*100) + (dezena*10) + unidade;
+    return invertido;
 }
 
 int main (){
-    int tam = 0, i = 0, x = 0, invertido = 0;
-    printf("digite quantas vezes vc quer testar: ");
+    int tam = 0, i = 0, x = 0,j = 0, l = 0, invertido = 0;
     scanf("%d", &tam);
-    for(i=0; i != tam; i++){
-        printf("digite o numero: ");
-        scanf("%d", x);
-        invertido = inversao(x);
-        printf("invertido: %d\n", invertido);
-        // if(num == invertido){
-        //     printf("S\n");
-        // }
-        // else{
-        //     printf("N\n");
-        // }
+    int aux[tam], aux2[tam];
+    for(i=0; i < tam; i++){
+        scanf("%d", &x);
+        aux[j] = x;
+        aux2[l] = inversao(x);
+
+        j++;
+        l++;
+    }
+    j = 0;
+    l = 0;
+    for(l=0; l < tam; l++){
+        if(aux[l] == aux2[j]){
+            printf("S\n");
+        }
+        else{
+            printf("N\n");
+        }
+        j++;
     }
     return 0;
 }
--- overflow.c
+++ overflow.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
 
+void comparando(int maiorNum, int result){
+    if(result <= maiorNum){
+        printf("OK\n");
+    }
+    else{
+        printf("OVERFLOW\n");
+    }
+}
+
 int main(){
     int maiorNum = 0, x = 0, y = 0, result = 0;
-    char operacao[2]; 
+    char operacao; 
     
-    printf("Digite o maior numero que o consegue representar: ");
     scanf("%d", &maiorNum);
-    printf("digite dois numeros e o caractere de operacao entre os numeros: ");
-    scanf("%d %c %d ", &x, &operacao, &y);
-    getchar();
-    // printf("%d %d\n", x, y);
-    printf(" '%c'\n", operacao);
-    if(operacao == "+"){
+    scanf("%d %c %d", &x, &operacao, &y);
+    
+    if(operacao == '+'){
         result = x + y;
-        printf("resultado: %d", result);
+        comparando(maiorNum, result);
     }
-    if(operacao == "*"){
+    
+    if(operacao == '*'){
         result = x * y;
-        printf("resultado: %d", result);
+        comparando(maiorNum, result);
     }
 
     return 0;
